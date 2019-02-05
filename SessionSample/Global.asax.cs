cbc8c5b7dbb96e3f03de89dcda883e95a871714d
Iain F <iain.forsgren@silvertech.com>
2019-02-05 16:55:18 -0500

    Created Session State example

--- SessionSample/Global.asax.cs
+++ SessionSample/Global.asax.cs
@@ -17,5 +17,19 @@ namespace SessionSample
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // If ASP MVC is being used then it is preferable to not use the actual Session
+            // object from HttpContext.Current.Session but to use the new
+            // HttpSessionStateWrapper& HttpSessionStateBase from System.Web.Abstractions.dll
+            // then use a Factory or DI to get the Session.
+            // https://stackoverflow.com/questions/560084/session-variables-in-asp-net-mvc/560115#comment372807_560115
+
+            var sessionWrapper = new HttpSessionStateWrapper(HttpContext.Current.Session)
+            {
+                { "DoNotTrack", true }
+            };
+        }
     }
 }
